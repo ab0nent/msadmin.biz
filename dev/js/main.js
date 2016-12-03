a7ce496f8e57ab049b1413f083148a23fdb21264
Oleksandr Demchenko <email.root@gmail.com>
2016-12-03 13:09:12 +0200

    master

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,13 +3,16 @@
  */
 
 var header = document.querySelector('.header');
+var btnUp = document.querySelector('.btn-up');
 
 window.onscroll = function () {
     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
     if(scrolled){
         header.classList.add('header_scrolled');
+        btnUp.classList.add('visible');
     }else{
         header.classList.remove('header_scrolled');
+        btnUp.classList.remove('visible');
     }
 };
